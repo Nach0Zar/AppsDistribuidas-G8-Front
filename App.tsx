aba95de130e4b8abbac440fb7528b663f4411199
ManuMarcos <manuelignaciomarcos@hotmail.com>
2024-05-29 01:24:07 -0300

    Merge with develop

--- App.tsx
+++ App.tsx
@@ -3,18 +3,12 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React, {useEffect} from 'react';
 import { Text } from "react-native"
 import SplashScreen from 'react-native-splash-screen';
-<<<<<<< HEAD
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+
 
 import Login from './ui/screens/Login';
 import NewUser from  './ui/screens/NewUser';
 import Home from  './ui/screens/Home';
-=======
-import { LoginScreen } from './app/ui/screens/LoginScreen.js';
-import { LoginNavigation, ProfileNavigation } from './app/navigation/Navigation';
-
->>>>>>> feature/profile-screen
 
 const Stack = createNativeStackNavigator();
 
@@ -24,7 +18,6 @@ const App = () => {
     SplashScreen.hide();
   }, []);
   
-<<<<<<< HEAD
   return (
     <>
       <NavigationContainer>
@@ -49,15 +42,11 @@ const App = () => {
       </NavigationContainer>
     </>
   );
-=======
-
-  return(
-    <NavigationContainer>
-      {/*<LoginNavigation/>*/}
+  {/*<NavigationContainer>
+      {/*<LoginNavigation/>
       <ProfileNavigation/>
     </NavigationContainer>
-  )
->>>>>>> feature/profile-screen
+  */}
 }
 
 export default App;
