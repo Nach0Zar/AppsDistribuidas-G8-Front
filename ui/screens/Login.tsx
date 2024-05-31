57ef932b23f6dc1d72a33c2eea27932f48e31c67
ManuMarcos <manuelignaciomarcos@hotmail.com>
2024-05-31 02:12:15 -0300

    Refactor login

--- ui/screens/Login.tsx
+++ ui/screens/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Text, SafeAreaView, TouchableOpacity, Image, View, Alert } from 'react-native';
+import { Text, SafeAreaView, TouchableOpacity, Image, View, Alert, ActivityIndicator } from 'react-native';
 import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import loginStyles from '../styles/loginStyles';
@@ -18,50 +18,67 @@ const GoogleLogin = async () => {
 const Login = () => {
   const [error, setError] = useState();
   const [isLoading, setIsLoading] = useState(false);
-
-
-  useEffect(() => {
-    
-  }, []);
-
   const navigation = useNavigation();
 
-  const signIn = async () => {
+  useEffect(() => {
+    isLoggedIn();
+  }, []);
 
+  const isLoggedIn = async () => {
+    setIsLoading(true)
+    try{
+      const jwt = await AsyncStorage.getItem('@sessionToken');
+      if(jwt != null){
+        console.log('Hay un jwt almacenado: ' + jwt)
+        navigation.dispatch(StackActions.replace(Routes.LandingStack));
+      }
+      console.log('No hay un jwt almacenado')
+      setIsLoading(false)
+    }
+    catch(error){
+      Alert.alert('Login Error', 'Error inesperado.');
+    }
+  }
+
+  
+  const signIn = async () => {
+    setIsLoading(true)
     try {
-      await GoogleSignin.hasPlayServices();
-      const userInfo = await GoogleSignin.signIn();
-      let config = {
+      const googleResponse = await GoogleLogin();
+      const {idToken} = googleResponse;
+      console.log(idToken)
+      console.log('Inicio llamado POST backend')
+      const backendResponse = await axios.post('https://apps-distribuidas-grupo-8.onrender.com/api/auths', {}, {
         headers: {
-          'Authorization': userInfo.idToken,
-          'Content-Type': 'application/json'
+          'Authorization' : idToken,
+          'Content-Type' : 'application/json'
         }
-      };
-      const response = await axios.post('https://apps-distribuidas-grupo-8.onrender.com/api/auths', {}, config);
-      const jwtToken = response.data
-      config = {
-        headers: {
+      });
+      const jwtToken = backendResponse.data
+      console.log('Inicio llamado Get backend')
+      const authedUserInformationResponse = await axios.get('https://apps-distribuidas-grupo-8.onrender.com/api/users',{
+        headers : {
           'Authorization': jwtToken,
           'Content-Type': 'application/json'
         }
-      };
-      const authedUserInformationResponse = await axios.get('https://apps-distribuidas-grupo-8.onrender.com/api/users', {}, config);
+      });
       const authedUserInformation = authedUserInformationResponse.data
       await AsyncStorage.clear()
       await AsyncStorage.setItem('@firstname', authedUserInformation.firstname || '');
       await AsyncStorage.setItem('@lastname', authedUserInformation.lastname  || '');
       await AsyncStorage.setItem('@email', authedUserInformation.email || '');
       await AsyncStorage.setItem('@image', authedUserInformation.image || '');
-      await AsyncStorage.setItem('@googleToken', userInfo.idToken || '');
+      await AsyncStorage.setItem('@googleToken', idToken || '');
       await AsyncStorage.setItem('@sessionToken', jwtToken || '');
-      if (response.status === 201) {
+      if (backendResponse.status === 201) {
+        console.log('Status 201')
         //navigation.navigate('NewUser') TODO: Para mi sacamos este stack
       } else {
         navigation.dispatch(
           StackActions.replace(Routes.LandingStack)
         );
       }
-    } catch (error:any) {
+    } catch (error : any) {
       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
         Alert.alert('Login cancelado', 'Cancelaste el proceso de login.');
       } else {
@@ -69,6 +86,8 @@ const Login = () => {
       }
     }
   };
+
+
   return (
     <>
       <SafeAreaView style={loginStyles.container}>
@@ -77,7 +96,7 @@ const Login = () => {
           source={require('../../assets/images/logo.png')}
         />
         <Text style={loginStyles.message}>Accede para ver el contenido</Text>
-        <TouchableOpacity
+        {isLoading ? <ActivityIndicator size="small" color="#0000ff" /> : <TouchableOpacity
           onPress={() => signIn()}
           style={loginStyles.googleButton}>
           <View style={{ flexDirection: 'row' }}>
@@ -85,6 +104,8 @@ const Login = () => {
             <Text style={loginStyles.googleButtonText}>Iniciar sesion</Text>
           </View>
         </TouchableOpacity>
+      }
+        
       </SafeAreaView>
     </>
   );
