106f3f38e10b3e14caafc0976a893cac924aeb9e
Nach0Zar <ignaciozarlenga@hotmail.com>
2024-06-07 20:00:44 -0300

    Changed token structure

--- ui/screens/Login.tsx
+++ ui/screens/Login.tsx
@@ -22,10 +22,10 @@ const Login = () => {
         }
       };
       const response = await axios.post('https://apps-distribuidas-grupo-8.onrender.com/api/auths', {}, config);
-      const jwtToken = response.data
+      const tokens = response.data
       config = {
         headers: {
-          'Authorization': jwtToken,
+          'Authorization': tokens.jwtToken,
           'Content-Type': 'application/json'
         }
       };
@@ -36,8 +36,8 @@ const Login = () => {
       await AsyncStorage.setItem('@lastname', authedUserInformation.lastname  || '');
       await AsyncStorage.setItem('@email', authedUserInformation.email || '');
       await AsyncStorage.setItem('@image', authedUserInformation.image || '');
-      await AsyncStorage.setItem('@accessToken', newTokens.accessToken || '');
-      await AsyncStorage.setItem('@sessionToken', jwtToken || '');
+      await AsyncStorage.setItem('@refreshToken', tokens.refreshToken || '');
+      await AsyncStorage.setItem('@sessionToken', tokens.jwtToken || '');
       // if (response.status === 201) {
       //   setLoggedIn(true);
       //   navigation.navigate('NewUser') TODO: Para mi sacamos este stack
