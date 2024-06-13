aed73a9dc52153553b4b7416c17f9c265603e1a8
Nach0Zar <ignaciozarlenga@hotmail.com>
2024-06-12 22:38:42 -0300

    removed console.logs

--- redux/slices/authActions.js
+++ redux/slices/authActions.js
@@ -6,12 +6,8 @@ import {Global} from '../../Constants';
 export const userLogin = createAsyncThunk(
   'auth/login',
   async () => {
-    console.log('Google Sign In');
     await GoogleSignIn.signIn();
     const newTokens = await GoogleSignIn.getTokens();
-    console.log('Google Access Token: ' + newTokens.accessToken);
-
-    console.log('Llamado al back /auths POST');
     const postAuthResponse = await axios.post(
       Global.BASE_URL + '/auths',
       {},
@@ -22,8 +18,6 @@ export const userLogin = createAsyncThunk(
         },
       },
     );
-    console.log('Jwt: ' + postAuthResponse.data.jwt);
-    console.log('Llamado al back /users GET');
     const getUserResponse = await axios.get(
       Global.BASE_URL + '/users',
       {
