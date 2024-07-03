b8b5fce4dc3fba318872a5ff266fc74c4c038ab3
Nach0Zar <ignaciozarlenga@hotmail.com>
2024-07-02 23:59:57 -0300

    Implemented favorite logic

--- ui/screens/Details.tsx
+++ ui/screens/Details.tsx
@@ -9,7 +9,11 @@ import { Global } from '../../Constants';
 import detailsStyle from '../styles/detailsStyle';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faAngleLeft, faHeart, faShareNodes } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
 import COLORS from '../styles/Theme';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, setUserToken, updateFavoritesList} from '../../redux/slices/authSlice';
+import InternalError from './errors/InternalError';
 
 export interface MovieProps {
   route: {
@@ -64,7 +68,80 @@ const Details = (props: MovieProps) => {
   const navigation = useNavigation();
   const [loadedMovie, setLoadedMovie] = useState<boolean>(false);
   const [movie, setMovie] = useState<Movie|null>(null);
+  const [favorite, setFavorite] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<String>("");
   const { id } = props.route.params;
+  const dispatch = useDispatch();
+  const {userInfo, userToken, refreshToken} = useSelector((state:any) => state.auth);
+  const loadProfileInfo = async () => {
+    navigation.dispatch(StackActions.replace(Routes.HomeScreen))
+  }
+  const handleRefreshToken = async () => {
+    try{
+      let connectionStatus = await ConnectionStatus()
+      if(!connectionStatus){
+        navigation.dispatch(StackActions.replace(Routes.InternetError));
+      }
+      const refreshTokenResponse = await axios.put(
+        Global.BASE_URL + '/auths',{
+        headers: {
+          "Authorization" : refreshToken,
+          "Content-Type" : "application/json"
+        }}
+      );
+      if(refreshTokenResponse.status === 200){
+        dispatch(setUserToken(refreshTokenResponse))
+      }
+    }catch(error){
+      dispatch(logout());
+    }
+  };
+  const handleFavorite = async () => {
+    let connectionStatus = await ConnectionStatus();
+    if(!connectionStatus){
+      navigation.dispatch(StackActions.replace(Routes.InternetError));
+    }
+    setFavorite(!favorite)
+    try{
+      let response;
+      if(!favorite){
+        response = await axios.post(
+          Global.BASE_URL + '/users/favorites/' + id,{},
+          {
+            headers: {
+              Authorization: userToken,
+              'Content-Type': 'application/json',
+            },
+          },
+        );
+      }
+      else{
+        response = await axios.delete(
+          Global.BASE_URL + '/users/favorites/' + id,{
+            headers: {
+              'Authorization' : userToken,
+              'Content-Type' : 'application/json'
+            }
+          }
+        );
+      }
+      let newList: any[] = [];
+      response.data.forEach((movie: any) => {
+        newList.push(movie.id)
+      })
+      dispatch(updateFavoritesList(newList))
+    }
+    catch(error: any) {
+      if(error.response && error.response.status === 403){
+        handleRefreshToken();
+      }
+      else{
+        setError(true)
+        setErrorMessage(`Mensaje: ${error.message} \nCodigo de error:  ${error.code}`)
+      }
+    }
+  }
   useEffect(() => {
     const getMovieInformation = async () => {
       let connectionStatus = await ConnectionStatus();
@@ -117,7 +194,7 @@ const Details = (props: MovieProps) => {
         duration = movieData.duration + " min"
       }
       let release_year = movieData.release_date.substring(0, 4);
-
+      (userInfo.favorites.includes(id)) && setFavorite(true);
       setMovie({
         id: movieData.id,
         title: movieData.title,
@@ -140,57 +217,65 @@ const Details = (props: MovieProps) => {
     if(!loadedMovie){
       getMovieInformation();
     }
-  }, [loadedMovie]);
+  }, [loadedMovie, favorite, error]);
 
   return (
-    <SafeAreaView style={detailsStyle.container}>
-    <View style={detailsStyle.header}>
-      <View style = {{flex: 1, flexBasis: '65%'}}>
-        <TouchableOpacity  onPress={() => navigation.goBack()}>
-          <FontAwesomeIcon icon={faAngleLeft} size={24} color={COLORS.white}/>
-        </TouchableOpacity>
-      </View>
-      {loadedMovie && 
-      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
-        <TouchableOpacity>
-          <FontAwesomeIcon icon={faHeart} size={24} color={COLORS.white}/>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <FontAwesomeIcon icon={faShareNodes} size={24} color={COLORS.white}/>
-        </TouchableOpacity>
+    <>
+      {(!error) ?
+      <SafeAreaView style={detailsStyle.container}>
+        <View style={detailsStyle.header}>
+          <View style = {{flex: 1, flexBasis: '65%'}}>
+            <TouchableOpacity  onPress={() => navigation.goBack()}>
+              <FontAwesomeIcon icon={faAngleLeft} size={24} color={COLORS.white}/>
+            </TouchableOpacity>
+          </View>
+          {loadedMovie && 
+          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+            <TouchableOpacity onPress={() => handleFavorite()}>
+              <FontAwesomeIcon icon={(favorite) ? faHeart : regularHeart} size={24} color={COLORS.white}/>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <FontAwesomeIcon icon={faShareNodes} size={24} color={COLORS.white}/>
+            </TouchableOpacity>
+          </View>
+          }
+        </View>
+        {loadedMovie ? 
+        <View style={detailsStyle.container}>
+          <View style={detailsStyle.imageContainer}>
+            <Image source={{uri: "https://image.tmdb.org/t/p/original" + movie!.default_poster.file_path}} style={detailsStyle.image}/>
+          </View>
+          <View style={detailsStyle.detailsContainer}>
+            <Text style={detailsStyle.title}>{movie!.title}</Text>
+            <Text style={detailsStyle.subtitle}>{movie!.subtitle}</Text>
+            <View style={detailsStyle.labelContainer}>
+              <Text style={detailsStyle.label}>{movie!.release_date}</Text>
+              <Text style={detailsStyle.label}>|</Text>
+              <Text style={detailsStyle.label}>{movie!.duration}</Text>
+              <Text style={detailsStyle.label}>|</Text>
+              <Text style={detailsStyle.label}>{movie!.genre.name}</Text>
+              <Text style={detailsStyle.label}>|</Text>
+              <Text style={detailsStyle.label}>{movie!.qualification}({movie!.qualifiers})</Text>
+            </View>
+            <View
+              style={detailsStyle.hr}
+            />
+            <Text style={detailsStyle.synopsis}>{movie!.synopsis}</Text>
+          </View>
+          <View style={detailsStyle.commentsAndPeopleContainer}>
+            <Text style={detailsStyle.synopsis}>asd</Text>
+          </View>
+        </View>
+        : 
+        <ActivityIndicator size="large" color={COLOR.second} style={{flex:1}}/>
+        }
+      </SafeAreaView>
+      :
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <InternalError onButtonClick={loadProfileInfo}/>
       </View>
       }
-    </View>
-    {loadedMovie ? 
-    <View style={detailsStyle.container}>
-      <View style={detailsStyle.imageContainer}>
-        <Image source={{uri: "https://image.tmdb.org/t/p/original" + movie!.default_poster.file_path}} style={detailsStyle.image}/>
-      </View>
-      <View style={detailsStyle.detailsContainer}>
-        <Text style={detailsStyle.title}>{movie!.title}</Text>
-        <Text style={detailsStyle.subtitle}>{movie!.subtitle}</Text>
-        <View style={detailsStyle.labelContainer}>
-          <Text style={detailsStyle.label}>{movie!.release_date}</Text>
-          <Text style={detailsStyle.label}>|</Text>
-          <Text style={detailsStyle.label}>{movie!.duration}</Text>
-          <Text style={detailsStyle.label}>|</Text>
-          <Text style={detailsStyle.label}>{movie!.genre.name}</Text>
-          <Text style={detailsStyle.label}>|</Text>
-          <Text style={detailsStyle.label}>{movie!.qualification}({movie!.qualifiers})</Text>
-        </View>
-        <View
-          style={detailsStyle.hr}
-        />
-        <Text style={detailsStyle.synopsis}>{movie!.synopsis}</Text>
-      </View>
-      <View style={detailsStyle.commentsAndPeopleContainer}>
-        <Text style={detailsStyle.synopsis}>asd</Text>
-      </View>
-    </View>
-    : 
-    <ActivityIndicator size="large" color={COLOR.second} style={{flex:1}}/>
-    }
-    </SafeAreaView>
+    </>
   );
 };
 
